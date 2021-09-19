9596f3032753a9324440cfca8bc14bce0a1f6ce0
tiptondt1998 <dylan.tipton@live.com>
2021-09-19 18:52:53 -0400

    can now run on local host

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {ApolloProvider} from '@apollo/react-hooks';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 import Home from './components/Home';
+
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem('id_token');
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    });
+  },
+  uri: 'http://localhost:3001/graphql',
+});
 
 function App() {
   return (
@@ -12,6 +24,7 @@ function App() {
        <div>
          <Switch>
            <Route exact path='/' component={Home}/>
+           tests
          </Switch>
        </div>
      </Router>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import {Img} from 'react-image'
 import './styles.css';
 const Home = () => {
   return (
     <div>
         <body>
         <div id='banner'>
-        <h1>TwinFlame</h1>
+        <img src="https://img3.foodyas.com/780/038/338180497800389.jpg"/>
+        </div>
+        <div id="home-body">
+
         </div>
         </body>
     </div>
